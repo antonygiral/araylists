3e08debb625adec420f0c05249952501d0b82b19
merardo <merardo@gmail.com>
2023-11-07 19:14:16 +0100

    araylist

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -1,7 +1,22 @@
 package com.example;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-    }
+    
+        ArrayList <string> PeliculasLibros<>();
+        PeliculasLibros.add("pinocho");
+        PeliculasLibros.add("mulan");
+        PeliculasLibros.add("matilda");
+        PeliculasLibros.add("casper");
+        PeliculasLibros.add("jhomper");
+
+        System.out.println("peliculas o libros favoritos");
+        
+        for (String titulo: PeliculasLibros){
+            System.out.`System.out.println(titulo);
+        }
+
 }
